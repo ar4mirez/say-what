1a338900c8b170df01b398a4ef69f100ed607ca5
Angel Ramirez <angel@cuemby.com>
2020-07-28 16:36:17 -0400

    updating api and manifests

--- api/index.js
+++ api/index.js
@@ -1,7 +1,25 @@
 const {send} = require('micro')
 const {router, get} = require('microrouter')
+const fetch = require('node-fetch')
 
-const i18n = async (req, res) => await send(res, 200, req.params)
+const services = {
+  es: process.env.ES_SERVICE,
+  fr: process.env.FR_SERVICE,
+  it: process.env.IT_SERVICE
+}
+
+const i18n = async (req, res) => {
+  try {
+    const url = `${services[req.params.lang]}/translate?word=${req.params}`
+    const response = await fetch(url)
+    const data = await response.json()
+
+    send(res, 200, data)
+  }catch(err) {
+    send(res, 400, { err })
+  }
+}
+
 const ping = async (req, res) => await send(res, 200, { data: 'pong' })
 
 module.exports = router(
